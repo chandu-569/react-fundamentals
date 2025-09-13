7ae3362b7479657f48e45505cc0852a82eebe1be
chandu-569 <chanduchintakayala2@gmail.com>
2025-09-13 09:48:46 +0530

    react

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,19 +1,29 @@
 import Profile from "./profile";
+import ProductCard from "./ProductCard";
 function App() {
 
 let c1="class"
 let name="chandu"
 let age="20"
 const isStudent=true;
+let productname="cars"
+const prize=10000000
+const isavailable=true
+
   return (
     
     <div>
       <Profile />
+      <ProductCard/> 
       <p>hello react</p>
       <p>first {c1}</p>
     <p> my name is {name}</p>
     <p>age:{age}</p>
     {isStudent===true?"student":"not student"}
+    <br />
+    <br />
+    <br />
+    <ProductCard productname={productname} prize={prize} isavailable={isavailable}/>
     </div>
 
   )
---
+++ react-practice/src/ProductCard.jsx
@@ -0,0 +1,10 @@
+function ProductCard({productname,prize,isavailable})
+{
+    return(
+        <div>
+            
+            <p>product name={productname} , prize={prize}{isavailable}</p>
+        </div>
+    )
+}
+export default ProductCard;
